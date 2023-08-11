fb76b6a014043190d742ae4516a8d7b501ee83e3
TaiveyonShaw <shawt6@wwu.edu>
2023-08-10 22:38:52 -0700

    Continued tutorial

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,39 +1,68 @@
 import { useState } from "react"
 
-function Square() {
-const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X')
-  }
-
+function Square({value, onSquareClick}) {
   return (
-    <button 
-    className="square"
-    onClick={handleClick}
-    >
-      {value}
-    </button>
+  <button className="square" onClick={onSquareClick}>
+    {value}
+  </button>
   )
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [square, setSquare] = useState(Array(9).fill(null))
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function handleClick(i) {
+    if (square[i] || calculateWinner(square)) {
+      return
+    }
+    const nextSquares = square.slice()
+    if (xIsNext) {
+      nextSquares[i] = 'X'
+    } else {
+      nextSquares[i] = 'O'
+    }
+    setSquare(nextSquares)
+    setXIsNext(!xIsNext)
+  }
+
+  // Can optimize by using CSS instead of div and for-loop
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
